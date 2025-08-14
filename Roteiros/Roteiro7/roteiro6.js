de3c9fd6968896732dfc5edd77768a11d3e5ad32
Antonio Barros <128563411+Antonio-BNeto@users.noreply.github.com>
2025-08-14 17:43:00 -0300

    resolução da questão 4

--- Roteiros/Roteiro7/roteiro6.js
+++ Roteiros/Roteiro7/roteiro6.js
@@ -38,3 +38,9 @@ console.log(joinObjs({nome: "TESTE", cpf: "123"}, {nome: "Gabriel", nomePai : "Q
 
 // ============ Questão 4 =============
 
+const recebeNListas = (...lista) => {
+   
+    return [...lista].flat().sort((a, b) => a - b);
+}
+
+console.log(recebeNListas([1, 3], [2, 4, 6], [5, 7, 8]))
