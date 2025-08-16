62b1e29151a2525f12042804ef0bfad178f1ca77
Antonio Barros <128563411+Antonio-BNeto@users.noreply.github.com>
2025-08-15 21:16:57 -0300

    Delete Roteiros/Roteiro7/roteiro6.js

--- Roteiros/Roteiro7/roteiro6.js
+++
@@ -1,9 +0,0 @@
-
-// ============ Questão 4 =============
-
-const recebeNListas = (...lista) => {
-   
-    return [...lista].flat().sort((a, b) => a - b);
-}
-
-console.log(recebeNListas([1, 3], [2, 4, 6], [5, 7, 8]))
